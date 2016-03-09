4d53fa7bf4cc4eb565efe7b434636660deed08f4
jruffian <jruffian114@gmail.com>
2016-03-10 01:15:01 +0800

    add a button

--- SimpleUI/app/src/main/java/tw/idv/crystalfish/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/tw/idv/crystalfish/simpleui/MainActivity.java
@@ -2,12 +2,24 @@ package tw.idv.crystalfish.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button)findViewById(R.id.button1);
     }
+
+    public void submit(View view)
+    {
+        Toast.makeText(this, "Hello world", Toast.LENGTH_LONG);
+    }
+
 }
